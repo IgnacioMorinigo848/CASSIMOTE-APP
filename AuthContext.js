ac5ce27db4cb5ff2f9fd0fc6ba965e630931b065
FacuBelli <121831015+FacuBelli@users.noreply.github.com>
2025-07-03 20:49:21 -0300

    Add files via upload

---
+++ AuthContext.js
@@ -0,0 +1,130 @@
+import React, { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import Constants from 'expo-constants';
+
+const API_URL = Constants.expoConfig.extra.EXPO_API_URL;
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true); // inicia en true hasta verificar token
+
+  const login = async (email, password, rememberMe = false) => {
+    const query = `
+      mutation signIn($email: String!, $password: String!) {
+        signIn(email: $email, password: $password) {
+          success
+          token
+          errors {
+            email
+            password
+          }
+        }
+      }
+    `;
+
+    const variables = { email, password };
+
+    try {
+      const response = await axios.post(
+        API_URL,
+        { query, variables },
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+
+      const data = response.data;
+
+      if (data.errors) {
+        throw new Error(data.errors[0]?.message || 'Error en la respuesta del servidor');
+      }
+
+      const resp = data.data.signIn;
+
+      if (resp.success) {
+        const userData = { email };
+        setToken(resp.token);
+        setUser(userData);
+
+        if (rememberMe) {
+          await AsyncStorage.setItem('token', resp.token);
+          await AsyncStorage.setItem('user', JSON.stringify(userData));
+        }
+
+        return {
+          success: true,
+          message: 'Logeado exitosamente.'
+        };
+      }
+
+      return {
+        success: false,
+        message: resp.errors
+      };
+
+    } catch (error) {
+      console.error('Login error:', error);
+      return {
+        success: false,
+        message: error.message || 'Error desconocido'
+      };
+    }
+  };
+
+  const logout = async () => {
+    await AsyncStorage.removeItem('token');
+    await AsyncStorage.removeItem('user');
+    await AsyncStorage.removeItem('rememberMe');
+    setToken(null);
+    setUser(null);
+    
+  };
+
+  const checkLoginStatus = async () => {
+  try {
+    const rememberMe = await AsyncStorage.getItem('rememberMe');
+    if (rememberMe === 'true') {
+      const storedToken = await AsyncStorage.getItem('token');
+      const storedUser = await AsyncStorage.getItem('user');
+
+      if (storedToken) {
+        setToken(storedToken);
+      }
+
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    } else {
+      // Si no quiso ser recordado, borramos todo
+      await AsyncStorage.multiRemove(['token', 'user']);
+      setToken(null);
+      setUser(null);
+    }
+  } catch (err) {
+    console.error('Error cargando estado de sesión:', err);
+  } finally {
+    setLoading(false);
+  }
+};
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{
+      user,
+      setUser,
+      token,
+      setToken,
+      loading,
+      login,
+      logout,
+      setToken
+    }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ mainStackNavigator.js
@@ -0,0 +1,52 @@
+import React, { useContext } from "react";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import Home from "../screens/home/home.jsx";
+import Welcome from "../screens/welcome/welcome.jsx";
+import Onboarding from "../screens/onboarding/Onboarding.jsx";
+import SignUpFlowStackNatigator from "../navigation/signUpFlowStackNavigator.js"
+import SingIn from "../screens/signIn/SignIn.jsx";
+import RecoverAccountFlowStackNavigator from "../navigation/recoverAccountFlowStackNavigator.js";
+import ProfileFlowStackNavigator from "../navigation/profileFlowStackNavigator.js";
+import FilteredResultsScreen from '../screens/filter/FilteredResultsScreen.jsx';
+import RecipeDetailScreen from '../screens/Recipes/RecipeDetailScreen';
+import AddRatingScreen from "../screens/Recipes/AddRatingScreen.jsx";
+import CreateRecipeFlowStackNavigator from "./createRecipeFlowStackNavigator.js"
+import ArchivedScreen from '../screens/archived/archivedScreen.jsx';
+import { AuthContext } from "../context/AuthContext.js";
+import { ActivityIndicator, View } from "react-native";
+import ApproverScreen from "../screens/approver/ApproverScreen.jsx";
+
+export default function mainStackNavigator() {
+    const {token,loading} = useContext(AuthContext);
+
+    const Stack = createNativeStackNavigator();
+
+    const getInitialRouteName = () =>{
+        return token ? "home" : "welcome"
+}
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+    return(
+        <Stack.Navigator initialRouteName={getInitialRouteName()}>
+            <Stack.Screen name="welcome" component={Welcome} options={{ headerShown: false }}/>
+            <Stack.Screen name="home" component={Home} options={{ headerShown: false }}/>
+            <Stack.Screen name="onboarding" component={Onboarding}/>
+            <Stack.Screen name="signIn" component={SingIn} options={{headerShown:false}}/>
+            <Stack.Screen name="signUpFlowStackNatigator" component={SignUpFlowStackNatigator} options={{headerShown:false}}/>
+            <Stack.Screen name="recoverAccountFlowStackNavigator" component={RecoverAccountFlowStackNavigator} options={{headerShown:false}}/>
+            <Stack.Screen name="profileFlowStackNavigator" component={ProfileFlowStackNavigator} options={{headerShown:false,gestureEnabled: false }}/>
+            <Stack.Screen name="filteredResults" component={FilteredResultsScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} options={{headerShown:false}}/>
+            <Stack.Screen name="AddRating" component={AddRatingScreen} options={{headerShown:false}}/>
+            <Stack.Screen name="createRecipe" component={CreateRecipeFlowStackNavigator} options={{headerShown:false}}/>
+            <Stack.Screen name="archived" component={ArchivedScreen} options={{headerShown:false}}/>
+            <Stack.Screen name="approver" component={ApproverScreen} options={{headerShown:false}}/>
+        </Stack.Navigator>
+    );
+};
